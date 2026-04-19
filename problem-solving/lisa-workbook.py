5f5634bb04df5a0d4b21b006a976a665c3486cda
marmag0 <mikolaj.mazur05@gmail.com>
2026-04-19 15:31:06 +0200

    lisa workbook challange solved

--- problem-solving/lisa-workbook.py
+++ problem-solving/lisa-workbook.py
@@ -8,10 +8,23 @@
 #  2. INTEGER k
 #  3. INTEGER_ARRAY arr
 
-def workbook(n, k, arr):
-    pass
+def workbook(n: int, k: int, arr: list) -> int:
+    s_pages: int = 0
+    cur_page: int = 1
+    
+    # iterate for each chapter
+    for i in range(n):
+        for problem_no in range(1, arr[i]+1):
+            if problem_no == cur_page:
+                s_pages += 1
+            if problem_no % k == 0:
+                cur_page += 1
+        if problem_no % k != 0:
+            cur_page += 1
+
+    return(s_pages)
 
 
 
 if __name__ == '__main__':
-    pass
+    print(workbook(n=2, k=3, arr=[4, 2]))
